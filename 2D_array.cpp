a62fa637743f5a5a5905d00e9808bcbd65a12ca4
Satyam <58383389+satyamgupta0202@users.noreply.github.com>
2020-10-01 21:24:08 +0530

    2D_array.cpp
    generalized it for n*n matrix with using swap from algorithm library

--- 2D_array.cpp
+++ 2D_array.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
+#include<algorithm>
 #include <stdio.h>
 using namespace std;
 int main (){    
 
-int x[4][4],i,k,j;
-for(i=0;i<4;i++)
-    for(j=0;j<4;j++)
+int i,k,j,n;
+cin>>n;
+int x[n][n];
+for(i=0;i<n;i++)
+    for(j=0;j<n;j++)
         cin>>x[i][j];
 
-cout<<"\n\n";
+cout<<endl;
 
-for(i=0;i<4;i++)
-    for(j=0;j<3;j++)
-        for(k=j+1;k<4;k++)
+for(i=0;i<n;i++)
+    for(j=0;j<n-1;j++)
+        for(k=j+1;k<n;k++)
             if(x[i][j]>x[i][k])
             {
-                int temp = x[i][j];
-                x[i][j] = x[i][k];
-                x[i][k] = temp;
+              swap(a[i][j],a[i][k]);
             }           
 
-for(i=0;i<4;i++)
+for(i=0;i<n;i++)
     {
-    for(j=0;j<4;j++)
+    for(j=0;j<n;j++)
         cout<<x[i][j]<<"\t";
-        cout<<"\n\n";       
+        cout<<endl;      
     }
 
 
